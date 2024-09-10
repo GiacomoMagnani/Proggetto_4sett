f98ef502af322fe644244fbd24d0ee11c5db0494
Davide Spataro <davide.spataro@chintai.io>
2024-09-10 18:07:25 +0200

    draft ordine

--- src/davide.c
+++ src/davide.c
@@ -84,7 +84,7 @@ typedef struct recipe_t
 {
     char *name;
     int weight;
-    int pending_orders;
+    int number_pending_orders;
     linked_list_ptr ingredients;
 } recipe;
 
@@ -119,9 +119,31 @@ typedef struct ingredient_t
 } ingredient;
 typedef ingredient *ingredient_ptr;
 
-
+typedef struct order
+{
+    recipe_ptr recipe;
+    int quantity;
+    int time;
+    int total_weight;
+} order;
+typedef order *order_ptr;
 
 // DEBUG FUNCTIONS
+void print_order_list(linked_list_ptr list, char *list_name)
+{
+    printf("Orders %s: ", list_name);
+    linked_list_node_ptr current_node = list->head;
+    while (current_node != NULL)
+    {
+        order_ptr current_order = (order_ptr)current_node->data;
+        printf("(name:%s, time: %d, qnt:%d, weight:%d)", current_order->recipe->name, current_order->time, current_order->quantity, current_order->total_weight);
+        current_node = current_node->next;
+        if (current_node)
+            printf(", ");
+    }
+    printf("\n");
+}
+
 void print_warehouse_item(warehouse_item_ptr item)
 {
     printf("Warehouse item: %s\n", item->name);
@@ -164,29 +186,33 @@ void print_recipe_ingredients(linked_list_ptr ingredients)
     while (current_ingredient != NULL)
     {
         ingredient_ptr current_ingredient_data = (ingredient_ptr)current_ingredient->data;
-        printf("(name: %s, qnt:%d)", current_ingredient_data->name,current_ingredient_data->quantity);
-        
-        //print_warehouse_item(current_ingredient_data->warehouse_item_info);
+        printf("(name: %s, qnt:%d)", current_ingredient_data->name, current_ingredient_data->quantity);
+
+        // print_warehouse_item(current_ingredient_data->warehouse_item_info);
         current_ingredient = current_ingredient->next;
-        if(current_ingredient)
+        if (current_ingredient)
             printf(", ");
     }
     printf("\n");
 }
 
-void print_recipes(hashtable_ricette_t hashtable_ricette){
+void print_recipes(hashtable_ricette_t hashtable_ricette)
+{
     printf("------RECIPES------\n");
-    for(int i = 0 ; i < HASHTABLE_SIZE; i++){
+    for (int i = 0; i < HASHTABLE_SIZE; i++)
+    {
         linked_list_ptr current_list = hashtable_ricette[i];
-        if(current_list->length <= 0){
+        if (current_list->length <= 0)
+        {
             continue;
         }
         linked_list_node_ptr current_recipe = current_list->head;
-        while(current_recipe != NULL){
+        while (current_recipe != NULL)
+        {
             recipe_ptr current_recipe_data = (recipe_ptr)current_recipe->data;
             printf("[debug] Recipe: %s\n", current_recipe_data->name);
             printf("[debug] Weight: %d\n", current_recipe_data->weight);
-            printf("[debug] Pending orders: %d\n", current_recipe_data->pending_orders);
+            printf("[debug] Pending orders: %d\n", current_recipe_data->number_pending_orders);
             printf("[debug] Ingredients:");
             print_recipe_ingredients(current_recipe_data->ingredients);
             printf("\n");
@@ -194,8 +220,6 @@ void print_recipes(hashtable_ricette_t hashtable_ricette){
         }
     }
 }
-
-
 
 hashtable_warehouse_t hashtable_warehouse;
 
@@ -212,7 +236,7 @@ hashtable create_hashtable()
 void *find_in_hashtable(hashtable hashtable, int (*name_match)(void *, char *), char *name)
 {
     size_t hash_indice = hash_string(name);
-    //printf("[debug] Finding: %s in hashtable at index %zu\n", name, hash_indice);
+    // printf("[debug] Finding: %s in hashtable at index %zu\n", name, hash_indice);
     linked_list_ptr current_recipe = hashtable[hash_indice];
     if (!current_recipe)
         return NULL;
@@ -237,7 +261,7 @@ int recipe_name_match(void *recipe, char *recipe_name)
 
 recipe_ptr find_recipe(hashtable_ricette_t hashtable_ricette, char *recipe_name)
 {
-    //printf("[debug] Finding recipe: %s\n", recipe_name);
+    // printf("[debug] Finding recipe: %s\n", recipe_name);
     recipe_ptr found_recipe = (recipe_ptr)find_in_hashtable(hashtable_ricette, recipe_name_match, recipe_name);
     return found_recipe;
 }
@@ -263,7 +287,7 @@ warehouse_item_ptr find_or_add_default_warehouse_item(hashtable_warehouse_t hash
         found_item->quantity_total_in_stock = 0;
         found_item->batches = linked_list_initialize();
         size_t hash_indice = hash_string(warehouse_item_name);
-        linked_list_push(hashtable_warehouse[hash_indice], found_item);
+        linked_list_push_back(hashtable_warehouse[hash_indice], found_item);
     }
     return found_item;
 }
@@ -273,6 +297,8 @@ hashtable_ricette_t hashtable_ricette;
 hashtable_warehouse_t hashtable_warehouse;
 int current_time = 1;
 int truck_capacity = 0;
+linked_list_ptr ready_orders;
+linked_list_ptr pending_orders;
 
 void consume_input()
 {
@@ -280,16 +306,17 @@ void consume_input()
     int printed = 0;
     while (!is_new_line(c))
     {
-        if(!printed){
-            //printf("[debug] Consuming: ");
+        if (!printed)
+        {
+            // printf("[debug] Consuming: ");
         }
-        //printf("%c", c);
+        // printf("%c", c);
         c = getchar();
         printed = 1;
     }
     if (printed)
     {
-        //printf("\n");
+        // printf("\n");
     }
 }
 
@@ -298,8 +325,7 @@ ingredient_ptr create_new_ingredient(char *name, int quantity)
     ingredient_ptr new_ingredient = (ingredient_ptr)malloc(sizeof(ingredient));
     new_ingredient->name = strdup(name);
     new_ingredient->quantity = quantity;
-    new_ingredient->warehouse_item_info = find_in_warehouse(hashtable_warehouse, name);
-
+    new_ingredient->warehouse_item_info = find_or_add_default_warehouse_item(hashtable_warehouse, name);
     return new_ingredient;
 }
 
@@ -308,7 +334,7 @@ recipe_ptr create_new_recipe(char *name, int weight, linked_list_ptr ingredients
     recipe_ptr new_recipe = (recipe_ptr)malloc(sizeof(recipe));
     new_recipe->name = strdup(name);
     new_recipe->weight = weight;
-    new_recipe->pending_orders = 0;
+    new_recipe->number_pending_orders = 0;
     new_recipe->ingredients = ingredients;
     return new_recipe;
 }
@@ -318,9 +344,8 @@ void add_new_recipe(hashtable_ricette_t hashtable_ricette, char *recipe_name, li
     size_t hash_indice = hash_string(recipe_name);
     linked_list_ptr current_list = hashtable_ricette[hash_indice];
     recipe_ptr new_recipe = create_new_recipe(recipe_name, total_recipe_quantity, ingredients);
-    linked_list_push(current_list, new_recipe);
+    linked_list_push_back(current_list, new_recipe);
 }
-
 
 void handle_add_recipe_command()
 {
@@ -348,27 +373,31 @@ void handle_add_recipe_command()
         }
         total_recipe_quantity += ingredient_quantity;
         ingredient_ptr new_ingredient = create_new_ingredient(ingredient_name, ingredient_quantity);
-        linked_list_push(ingredients, new_ingredient);
-        //printf("[debug] ingredient: '%s' quantity '%s' '%d'\n", recipe_name, ingredient_name, ingredient_quantity);
+        linked_list_push_back(ingredients, new_ingredient);
+        // printf("[debug] ingredient: '%s' quantity '%s' '%d'\n", recipe_name, ingredient_name, ingredient_quantity);
     }
     add_new_recipe(hashtable_ricette, recipe_name, ingredients, total_recipe_quantity);
-    //printf("[debug] Recipe DONE: %s\n", recipe_name);
+    // printf("[debug] Recipe DONE: %s\n", recipe_name);
     print_recipes(hashtable_ricette);
 }
 
-
-void free_ingredient(void* raw_ingredient)
+void free_ingredient(void *raw_ingredient)
 {
     ingredient_ptr ingredient = (ingredient_ptr)raw_ingredient;
     free(ingredient->name);
     free(ingredient);
 }
-void free_recipe(void* raw_recipe)
+void free_recipe(void *raw_recipe)
 {
     recipe_ptr recipe = (recipe_ptr)raw_recipe;
     free(recipe->name);
     linked_list_destroy(recipe->ingredients, free_ingredient);
     free(recipe);
+}
+void free_batch(void *raw_batch)
+{
+    item_batch_ptr batch = (item_batch_ptr)raw_batch;
+    free(batch);
 }
 
 void remove_recipe(hashtable_ricette_t hashtable_ricette, recipe_ptr recipe_to_remove)
@@ -378,7 +407,8 @@ void remove_recipe(hashtable_ricette_t hashtable_ricette, recipe_ptr recipe_to_r
     linked_list_remove_by_name(current_list, recipe_name_match, recipe_to_remove->name, free_recipe);
 }
 
-void handle_remove_recipe_command(){
+void handle_remove_recipe_command()
+{
     char recipe_name[MAXIMUM_IDENTIFIER_LENGHT];
     if (scanf("%s", recipe_name) != 1)
     {
@@ -390,7 +420,8 @@ void handle_remove_recipe_command(){
         printf("non presente\n");
         return;
     }
-    if(found_recipe->pending_orders > 0){
+    if (found_recipe->number_pending_orders > 0)
+    {
         printf("ordini in sospeso\n");
         return;
     }
@@ -399,10 +430,99 @@ void handle_remove_recipe_command(){
     printf("rimossa\n");
 }
 
+order_ptr create_new_order(recipe_ptr recipe, int quantity)
+{
+    order_ptr new_order = (order_ptr)malloc(sizeof(order));
+    new_order->recipe = recipe;
+    new_order->quantity = quantity;
+    new_order->time = current_time;
+    new_order->total_weight = quantity * recipe->weight;
+    return new_order;
+}
+
+void delete_all_expired_batches(warehouse_item_ptr item)
+{
+    printf("[debug] Deleting expired batches for ingredient %s\n", item->name);
+    linked_list_ptr batch_list = item->batches;
+    while (batch_list->head != NULL)
+    {
+        item_batch_ptr current_batch_data = (item_batch_ptr)batch_list->head->data;
+        if (current_batch_data->expiration_date <= current_time)
+        {
+            item->quantity_total_in_stock -= current_batch_data->quantity;
+            linked_list_pop_front(batch_list, free_batch);
+        }
+    }
+}
+
+int is_order_processable_now(order_ptr order)
+{
+    {
+        linked_list_node_ptr current_ingredient = order->recipe->ingredients->head;
+        while (current_ingredient != NULL)
+        {
+            ingredient_ptr current_ingredient_data = (ingredient_ptr)current_ingredient->data;
+            delete_all_expired_batches(current_ingredient_data->warehouse_item_info);
+            current_ingredient = current_ingredient->next;
+        }
+    }
+
+    linked_list_node_ptr current_ingredient = order->recipe->ingredients->head;
+    while (current_ingredient != NULL)
+    {
+        const int recipe_ingredient_quantity = ((ingredient_ptr)current_ingredient->data)->quantity;
+        const int order_ingredient_required_quantity = order->quantity * recipe_ingredient_quantity;
+        const int warehouse_ingredient_quantity = ((ingredient_ptr)current_ingredient->data)->warehouse_item_info->quantity_total_in_stock;
+        if (order_ingredient_required_quantity > warehouse_ingredient_quantity)
+        {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+void update_warehouse_on_order_processed(order_ptr order)
+{
+}
+
+void handle_add_order_command()
+{
+    char recipe_name[MAXIMUM_IDENTIFIER_LENGHT];
+    int order_quantity;
+    if (scanf("%s %d", recipe_name, &order_quantity) != 2)
+    {
+        panic("Invalid input");
+    }
+    recipe_ptr found_recipe = find_recipe(hashtable_ricette, recipe_name);
+    if (!found_recipe)
+    {
+        printf("rifiutato\n");
+        return;
+    }
+    printf("accettato\n");
+    found_recipe->number_pending_orders++;
+    order_ptr new_order = create_new_order(found_recipe, order_quantity);
+    print_entire_warehouse(hashtable_warehouse);
+    if (is_order_processable_now(new_order))
+    {
+        update_warehouse_on_order_processed(new_order);
+        linked_list_push_back(ready_orders, new_order);
+    }
+    else
+    {
+        linked_list_push_back(pending_orders, new_order);
+        print_order_list(pending_orders, "pending");
+    }
+    // printf("[debug] Order: %s %d\n", recipe_name, order_quantity);
+    // print_recipes(hashtable_ricette);
+}
+
 int main()
 {
     hashtable_ricette = create_hashtable();
     hashtable_warehouse = create_hashtable();
+    ready_orders = linked_list_initialize();
+    pending_orders = linked_list_initialize();
     int truck_loading_interval = 0;
     char raw_command[MAXIMUM_COMMAND_LENGHT];
 
@@ -417,7 +537,7 @@ int main()
 
     while (scanf("%s", raw_command) == 1)
     {
-        //printf("[debug] Command: %s\n", raw_command);
+        // printf("[debug] Command: %s\n", raw_command);
         switch (get_command(raw_command))
         {
         case ADD_RECIPE:
@@ -430,7 +550,7 @@ int main()
             printf("REFILL\n");
             break;
         case ORDER:
-            printf("ORDER\n");
+            handle_add_order_command();
             break;
         default:
             panic("Invalid command");
--- src/linked_list.h
+++ src/linked_list.h
@@ -42,7 +42,25 @@ linked_list_node_ptr linked_list_node_create(void *data)
     return node;
 }
 
-int linked_list_push(linked_list_ptr list, void *data)
+int linked_list_push_front(linked_list_ptr list, void *data)
+{
+    linked_list_node_ptr newNode = linked_list_node_create(data);
+
+    if (list->length == 0)
+    {
+        list->tail = newNode;
+    }
+    else
+    {
+        list->head->previous = newNode;
+        newNode->next = list->head;
+    }
+    list->head = newNode;
+    list->length++;
+    return OK;
+}
+
+int linked_list_push_back(linked_list_ptr list, void *data)
 {
     linked_list_node_ptr newNode = linked_list_node_create(data);
 
@@ -50,14 +68,13 @@ int linked_list_push(linked_list_ptr list, void *data)
     if (list->length == 0)
     {
         list->tail = newNode;
+        list->head = newNode;
+        list->length++;
+        return OK;
     }
-    else
-    {
-        /* if there is at least one element */
-        list->head->previous = newNode;
-        newNode->next = list->head;
-    }
-    list->head = newNode;
+    list->tail->next = newNode;
+    newNode->previous = list->tail;
+    list->tail = newNode;
     list->length++;
     return OK;
 }
@@ -95,6 +112,60 @@ int linked_list_remove_by_name(linked_list_ptr list, int (*predicate)(void *, ch
     free_data(current->data);
     free(current);
     return OK;
+}
+
+linked_list_node_ptr find_at_position(linked_list_ptr list, int position)
+{
+    if (position < 0 || position >= list->length)
+    {
+        return NULL;
+    }
+    linked_list_node_ptr current = list->head;
+    for (int i = 0; i < position; i++)
+    {
+        current = current->next;
+    }
+    return current;
+}
+
+int linked_list_remove_at_position(linked_list_ptr list, int position, void (*free_data)(void *))
+{
+    if (position < 0 || position >= list->length)
+    {
+        return NOK;
+    }
+    linked_list_node_ptr current = find_at_position(list, position);
+    if (current == NULL)
+    {
+        return NOK;
+    }
+
+    if (current->previous == NULL)
+    {
+        list->head = current->next;
+    }
+    else
+    {
+        current->previous->next = current->next;
+    }
+
+    if (current->next == NULL)
+    {
+        list->tail = current->previous;
+    }
+    else
+    {
+        current->next->previous = current->previous;
+    }
+    list->length--;
+    free_data(current->data);
+    free(current);
+    return OK;
+}
+
+int linked_list_pop_front(linked_list_ptr list, void (*free_data)(void *))
+{
+  return linked_list_remove_at_position(list, 0, free_data);
 }
 
 int linked_list_destroy(linked_list_ptr list, void (*free_data)(void *))
